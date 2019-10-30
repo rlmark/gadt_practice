0694b0fb697de7ab390994c6f35a07aae62a42a0
rebeccamark <rebecca.mark@47deg.com>
2019-10-30 11:10:44 -0700

    Initial commit

---
+++ src/main/scala/Lambda.scala
@@ -0,0 +1,10 @@
+trait Lambda {
+
+  trait Exp[T]
+  // Useful for embedding arbitrary things from parent language
+  case class Const[T](t: T) extends Exp[T]
+  // Abstraction
+  case class App[S, T](fun: Exp[S => T], arg: Exp[S]) extends Exp[T]
+  // Application
+  case class Fun[S, T](body: Exp[S] => Exp[T])extends Exp[S => T]
+}
